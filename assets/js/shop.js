51f8572179a46ebfe22d6fb57a2f13fb3a4236ca
Callum Newsome <39914113+callumndev@users.noreply.github.com>
2025-04-25 19:52:14 +0100

    Use local images

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -99,63 +99,63 @@ const inventory = {
             price: 5990,
             title: "2015 Vauxhall Insignia",
             tags: ["2015", "225,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1745480928299-3a0bae69-b5cb-4b48-ac84-e3e8e9ca6ae5"
+            image: "/assets/images/cars/1745480928299-3a0bae69-b5cb-4b48-ac84-e3e8e9ca6ae5.jpeg"
         },
         {
             type: "car",
             price: 7750,
             title: "2014 Hyundai IX35",
             tags: ["2014", "257,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1745479372225-875116e6-e87b-46eb-ba8e-93f86009e9ff"
+            image: "/assets/images/cars/1745479372225-875116e6-e87b-46eb-ba8e-93f86009e9ff.jpeg"
         },
         {
             type: "car",
             price: 7995,
             title: "2014 Volkswagen Golf",
             tags: ["2014", "226,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1745419377756-99688de1-9b8f-4470-bdbb-152dcd5b388a"
+            image: "/assets/images/cars/1745419377756-99688de1-9b8f-4470-bdbb-152dcd5b388a.jpeg"
         },
         {
             type: "car",
             price: 15850,
             title: "2021 Seat Ibiza",
             tags: ["2021", "74,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1744661528213-95d6ee49-5215-436a-8824-2f1f5689f500"
+            image: "/assets/images/cars/1744661528213-95d6ee49-5215-436a-8824-2f1f5689f500.jpeg"
         },
         {
             type: "car",
             price: 8990,
             title: "2014 Honda CR-V",
             tags: ["2014", "239,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1743166539451-be27da35-378d-4c12-bf69-eb56916d1357.jpg"
+            image: "/assets/images/cars/1743166539451-be27da35-378d-4c12-bf69-eb56916d1357.jpg"
         },
         {
             type: "car",
             price: 16500,
             title: "2017 Mercedes-Benz CLA",
             tags: ["2017", "199,000 km", "Automatic"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1743166514504-35723598-83f7-4102-b431-68f0dfba369a.jpg"
+            image: "/assets/images/cars/1743166514504-35723598-83f7-4102-b431-68f0dfba369a.jpg"
         },
         {
             type: "car",
             price: 4770,
             title: "2013 Nissan Qashqai+2",
             tags: ["2013", "231,458 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1743166519261-51bc83e7-1c89-4d5d-8b87-62933ff6731d.jpg"
+            image: "/assets/images/cars/1743166519261-51bc83e7-1c89-4d5d-8b87-62933ff6731d.jpg"
         },
         {
             type: "car",
             price: 9990,
             title: "2016 Volkswagen Passat",
             tags: ["2016", "225,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1743166535005-61943357-6f7f-4bb2-a41f-57b656a8d6d9.jpg"
+            image: "/assets/images/cars/1743166535005-61943357-6f7f-4bb2-a41f-57b656a8d6d9.jpg"
         },
         {
             type: "car",
             price: 8990,
             title: "2017 Peugeot 508",
             tags: ["2017", "221,000 km", "Manual"],
-            image: "https://cdn-api.kobos.io/dealer-car-images/1743166501186-05135d42-bfd7-4e08-80f3-28dbeaa747f9.jpg"
+            image: "/assets/images/cars/1743166501186-05135d42-bfd7-4e08-80f3-28dbeaa747f9.jpg"
         }
     ],
     car_part: [
@@ -164,63 +164,63 @@ const inventory = {
             name: "Lion AGM Stop/Start 019 95AH 850CCA Car Battery - 3 year Guarantee",
             productID: "444777001",
             price: 189.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/444777001/?guid=ztc6puxzh91kbz6yihynj4c5e86trd5uh6fd37e&currentVersion=1722960121000",
+            image: "/assets/images/car_parts/444777001.webp",
         },
         {
             type: "car_part",
             name: "NGK Spark Plug",
             productID: "408110310",
             price: 14.09,
-            image: "https://lkqmedia.carparts4less.co.uk/product/408110310/?guid=ztc6puxzh91kb595s8dsfgrm69sw4og4sijn1ja&currentVersion=1716861420000",
+            image: "/assets/images/car_parts/408110310.webp",
         },
         {
             type: "car_part",
             name: "Shell Helix Ultra 0W-20 ECT C6 1Ltr",
             productID: "521770192",
             price: 12.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/521770192/?guid=ztc6puxzh91kyjkmj6nrtkxnnoz83nikbuzzuzy&currentVersion=1722960356000",
+            image: "/assets/images/car_parts/521770192.webp",
         },
         {
             type: "car_part",
             name: "Wynns Hydraulic Fluid CHF11S 1Ltr",
             productID: "555770310",
             price: 25.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/555770310/?guid=ztc6puxzh91kbzwg8hosu8xeqyz83nikbuzzuzy&currentVersion=1715787235000",
+            image: "/assets/images/car_parts/555770310.webp",
         },
         {
             type: "car_part",
             name: "Mahle Oil Filter",
             productID: "501110104",
             price: 19.49,
-            image: "https://lkqmedia.carparts4less.co.uk/product/501110104/?guid=ztc6puxzh91kyg911u4fse5khfpgncu43qza3fy&currentVersion=1716857636000",
+            image: "/assets/images/car_parts/501110104.webp",
         },
         {
             type: "car_part",
             name: "Bosch Air Filter",
             productID: "502110187",
             price: 34.19,
-            image: "https://lkqmedia.carparts4less.co.uk/product/502110187/?guid=ztc6puxzh91kbytqozr3br1mcfrxye9cak8rnna&currentVersion=1723035129000",
+            image: "/assets/images/car_parts/502110187.webp",
         },
         {
             type: "car_part",
             name: "Bilstein Shock Absorber (Front)",
             productID: "635112645",
             price: 130.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/635112645/?guid=ztc6puxzh91kbw1xb7a3bj4c68hitc8msdhumpy&currentVersion=1714138862000",
+            image: "/assets/images/car_parts/635112645.webp",
         },
         {
             type: "car_part",
             name: "Sachs Coil Spring (Front)",
             productID: "627113170",
             price: 85.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/627113170/?guid=ztc6puxzh91kbmc77wjfdcmxcabzmkymo3fy3sy&currentVersion=1716911164000",
+            image: "/assets/images/car_parts/627113170.webp",
         },
         {
             type: "car_part",
             name: "Macht Mirror Cover (Left)",
             productID: "489111831",
             price: 43.49,
-            image: "https://lkqmedia.carparts4less.co.uk/product/489111831/?guid=ztc6puxzh91kypjizb3zi43x9rf1r1pebyxcsaa&currentVersion=1715374725000",
+            image: "/assets/images/car_parts/489111831.webp",
         },
     ],
     accessory: [
@@ -228,55 +228,55 @@ const inventory = {
             type: "accessory",
             name: "Some Top Tech 1800W Pressure Washer 135 Bar",
             price: 59.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/529771511/?guid=ztc6puxzh91kyd3uyx368y7gcqctwz6cupjugpy&currentVersion=1715767216000",
+            image: "/assets/images/accessories/529771511.webp",
         },
         {
             type: "accessory",
             name: "Martin Cox Standard Jumbo Sponge 200 x 120 x 50mm",
             price: 0.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/553000780/?guid=ztc6puxzh91kbytqozr3br1mcrdh8hint6q1hqe&currentVersion=1723035240000",
+            image: "/assets/images/accessories/553000780.webp",
         },
         {
             type: "accessory",
             name: "Top Tech Snow Foam Lance",
             price: 16.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/552999590/?guid=ztc6puxzh91ky4w8qjrq6j6rub51ofhrane3pje&currentVersion=1731675524000",
+            image: "/assets/images/accessories/552999590.webp",
         },
         {
             type: "accessory",
             name: "Martin Cox 20 Litre White Detailing/Valeting Bucket",
             price: 5.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/552980430/?guid=ztc6puxzh91kbirr7yg5js4xrxkk74dp4k5cimo&currentVersion=1734437239000",
+            image: "/assets/images/accessories/552980430.webp",
         },
         {
             type: "accessory",
             name: "Go4 Auto Air Con Cleaner 150ml",
             price: 3.69,
-            image: "https://lkqmedia.carparts4less.co.uk/product/552777691/?guid=ztc6puxzh91kyhus3fddpbuaeipgncu43qza3fy&currentVersion=1716846490000",
+            image: "/assets/images/accessories/552777691.webp",
         },
         {
             type: "accessory",
             name: "Osram NIGHT BREAKER H7 +220%",
             price: 29.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/481770224/?guid=ztc6puxzh91kbt6t48y9exrmpbetgznwrfsa1ce&currentVersion=1728470805000",
+            image: "/assets/images/accessories/481770224.webp",
         },
         {
             type: "accessory",
             name: "Stoplock Pro Elite Steering Wheel Lock",
             price: 47.99,
-            image: "https://lkqmedia.carparts4less.co.uk/product/553990190/?guid=ztc6puxzh91kyzm7xzfk5aswjypthos33ngeieo&currentVersion=1745399877000",
+            image: "/assets/images/accessories/553990190.webp",
         },
         {
             type: "accessory",
             name: "California Scents - Car Scents Coronado Cherry",
             price: 4.49,
-            image: "https://lkqmedia.carparts4less.co.uk/product/550770651/?guid=ztc6puxzh91kbn771u5qsso78y1cq9kaimzrs9o&currentVersion=1737630093000",
+            image: "/assets/images/accessories/550770651.webp",
         },
         {
             type: "accessory",
             name: "Carplan Oil Drain Can 9Ltr",
             price: 14.89,
-            image: "https://lkqmedia.carparts4less.co.uk/product/553772120/?guid=ztc6puxzh91kbtw3wor79xgqdhkqcmm9o3j3m8y&currentVersion=1715194345000",
+            image: "/assets/images/accessories/553772120.webp",
         },
     ],
 };
